6e7d72e876abac069faa161bdc2002824467b7a0
Juan Carlos Guerrero Davila <guerrero.juan.aprogm@gmail.com>
2025-04-30 20:13:00 -0600

    Update main.rs
    1

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,21 +1,20 @@
-#[macro_use] 
+#[macro_use]
 extern crate rocket;
 
 mod db;
 mod handler;
 
 use rocket::{launch, Build, Rocket};
-use sqlx::MySqlPool;
 use handler::{obtener_rutina_handler, insertar_confirmacion_handler, obtener_confirmacion_handler};
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
-    // Configuración del pool de conexiones con manejo de errores
-    let db_pool = db::create_db_pool().await.expect("Failed to create database pool");
+    let db_pool = db::create_db_pool()
+        .await
+        .expect("Failed to create database pool");
 
-    // Configuración de Rocket
     rocket::build()
-        .manage(db_pool) // Pool disponible en todos los handlers
+        .manage(db_pool)
         .mount("/", routes![
             obtener_rutina_handler,
             insertar_confirmacion_handler,
